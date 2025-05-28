927ca7b3f88b02c902e1900bac43a72da3ba5427
Aleksander <poczta@zielar.pl>
2025-05-28 18:02:46 +0200

    Update ripemd160_avx512.h

--- ripemd160_avx512.h
+++ ripemd160_avx512.h
@@ -6,13 +6,13 @@
 
 namespace ripemd160avx512 {
 
-// Initialize RIPEMD-160 state for 16 parallel computations
+// Initialize RIPEMD160 state using AVX-512
 void Initialize(__m512i *state);
 
-// Transform function using AVX-512
+// Transform function using AVX-512 for 16 parallel computations
 void Transform(__m512i *state, uint8_t *blocks[16]);
 
-// Hash 16 messages of 32 bytes each in parallel
+// Main RIPEMD160 function for 16 parallel 32-byte messages
 void ripemd160avx512_32(
     unsigned char *i0, unsigned char *i1, unsigned char *i2, unsigned char *i3,
     unsigned char *i4, unsigned char *i5, unsigned char *i6, unsigned char *i7,
@@ -22,6 +22,19 @@ void ripemd160avx512_32(
     unsigned char *d4, unsigned char *d5, unsigned char *d6, unsigned char *d7,
     unsigned char *d8, unsigned char *d9, unsigned char *d10, unsigned char *d11,
     unsigned char *d12, unsigned char *d13, unsigned char *d14, unsigned char *d15);
+
+// Backward compatibility wrapper for 8 parallel computations
+void ripemd160avx512_8B(
+    unsigned char *i0, unsigned char *i1, unsigned char *i2, unsigned char *i3,
+    unsigned char *i4, unsigned char *i5, unsigned char *i6, unsigned char *i7,
+    unsigned char *d0, unsigned char *d1, unsigned char *d2, unsigned char *d3,
+    unsigned char *d4, unsigned char *d5, unsigned char *d6, unsigned char *d7);
+
+// Batch processing for variable number of inputs
+void ripemd160avx512_batch(
+    unsigned char **input_array,
+    unsigned char **output_array,
+    int count);
 
 }  // namespace ripemd160avx512
 
