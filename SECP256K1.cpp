a1f94a421d96471d1a72672df0bf6769428fecb0
Aleksander <poczta@zielar.pl>
2025-05-28 17:54:59 +0200

    Update SECP256K1.cpp

--- SECP256K1.cpp
+++ SECP256K1.cpp
@@ -1,21 +1,9 @@
-#include <immintrin.h>
-#include <omp.h>
 #include <string.h>
+#include <immintrin.h>
 
 #include "SECP256K1.h"
 
-Secp256K1::Secp256K1() {
-// Ustaw optymalną liczbę wątków dla Xeon Platinum 8488C
-#pragma omp parallel
-  {
-#pragma omp master
-    {
-      // Dostosuj liczbę wątków do aktualnej liczby dostępnych procesorów
-      int num_threads = omp_get_num_procs();
-      omp_set_num_threads(num_threads);
-    }
-  }
-}
+Secp256K1::Secp256K1() {}
 
 void Secp256K1::Init() {
   // Prime for the finite field
@@ -33,26 +21,35 @@ void Secp256K1::Init() {
 
   Int::InitK1(&order);
 
-// Compute Generator table - zrównoleglone dla Xeon Platinum 8488C
-#pragma omp parallel
-  {
-#pragma omp single
-    {
-      Point N(G);
-      for (int i = 0; i < 32; i++) {
-        GTable[i * 256] = N;
-        N = DoubleDirect(N);
-
-#pragma omp taskloop
-        for (int j = 1; j < 255; j++) {
-          Point localN = N;
-          GTable[i * 256 + j] = AddDirect(localN, GTable[i * 256]);
-        }
-
-        GTable[i * 256 + 255] = N;  // Dummy point for check function
-      }
+  // Compute Generator table with AVX-512 optimizations
+  Point N(G);
+  for (int i = 0; i < 32; i++) {
+    // Prefetch next table entries for better cache performance
+    if (i < 31) {
+      _mm_prefetch((char*)&GTable[(i + 1) * 256], _MM_HINT_T1);
     }
+    
+    GTable[i * 256] = N;
+    N = DoubleDirect(N);
+    
+    for (int j = 1; j < 255; j++) {
+      // Prefetch ahead for cache optimization
+      if (j % 8 == 0 && j + 8 < 255) {
+        _mm_prefetch((char*)&GTable[i * 256 + j + 8], _MM_HINT_T0);
+      }
+      
+      GTable[i * 256 + j] = N;
+      N = AddDirect(N, GTable[i * 256]);
+      
+      // Apply fast reduction periodically for better performance
+      N.x.ModReduceK1AVX512();
+      N.y.ModReduceK1AVX512();
+    }
+    GTable[i * 256 + 255] = N;  // Dummy point for check function
   }
+  
+  // Memory fence to ensure table initialization is complete
+  _mm_mfence();
 }
 
 Secp256K1::~Secp256K1() {}
@@ -65,7 +62,12 @@ Point Secp256K1::AddDirect(Point &p1, Point &p2) {
   Point r;
   r.z.SetInt32(1);
 
-  // Obliczenia zoptymalizowane dla AVX-512
+  // Prefetch input data for better cache performance
+  _mm_prefetch((char*)p1.x.bits64, _MM_HINT_T0);
+  _mm_prefetch((char*)p1.y.bits64, _MM_HINT_T0);
+  _mm_prefetch((char*)p2.x.bits64, _MM_HINT_T0);
+  _mm_prefetch((char*)p2.y.bits64, _MM_HINT_T0);
+
   dy.ModSub(&p2.y, &p1.y);
   dx.ModSub(&p2.x, &p1.x);
   dx.ModInv();
@@ -80,12 +82,15 @@ Point Secp256K1::AddDirect(Point &p1, Point &p2) {
   r.y.ModMulK1(&_s);
   r.y.ModSub(&p2.y);  // ry = - p2.y - s*(ret.x-p2.x);
 
+  // Apply fast reduction for secp256k1
+  r.x.ModReduceK1AVX512();
+  r.y.ModReduceK1AVX512();
+
   return r;
 }
 
 Point Secp256K1::Add2(Point &p1, Point &p2) {
   // P2.z = 1
-  // Zoptymalizowana wersja dla Xeon Platinum 8488C
 
   Int u;
   Int v;
@@ -101,42 +106,19 @@ Point Secp256K1::Add2(Point &p1, Point &p2) {
   Int _2vs2v2;
   Point r;
 
-// Rozpocznij przetwarzanie równoległe dla niezależnych obliczeń
-#pragma omp parallel sections
-  {
-#pragma omp section
-    {
-      u1.ModMulK1(&p2.y, &p1.z);
-      v1.ModMulK1(&p2.x, &p1.z);
-    }
+  // Prefetch input data
+  _mm_prefetch((char*)p1.x.bits64, _MM_HINT_T0);
+  _mm_prefetch((char*)p1.y.bits64, _MM_HINT_T0);
+  _mm_prefetch((char*)p1.z.bits64, _MM_HINT_T0);
+  _mm_prefetch((char*)p2.x.bits64, _MM_HINT_T0);
+  _mm_prefetch((char*)p2.y.bits64, _MM_HINT_T0);
 
-#pragma omp section
-    {
-      // Przygotuj inne zmienne potrzebne w następnych krokach
-      us2.SetInt32(0);
-      vs2.SetInt32(0);
-    }
-  }
-
+  u1.ModMulK1(&p2.y, &p1.z);
+  v1.ModMulK1(&p2.x, &p1.z);
   u.ModSub(&u1, &p1.y);
   v.ModSub(&v1, &p1.x);
-
-// Wykorzystanie instrukcji AVX-512 do równoległego przetwarzania
-#pragma omp parallel sections
-  {
-#pragma omp section
-    {
-      us2.ModSquareK1(&u);
-      vs2.ModSquareK1(&v);
-    }
-
-#pragma omp section
-    {
-      vs3.SetInt32(0);
-      us2w.SetInt32(0);
-    }
-  }
-
+  us2.ModSquareK1(&u);
+  vs2.ModSquareK1(&v);
   vs3.ModMulK1(&vs2, &v);
   us2w.ModMulK1(&us2, &p1.z);
   vs2v2.ModMulK1(&vs2, &p1.x);
@@ -144,7 +126,6 @@ Point Secp256K1::Add2(Point &p1, Point &p2) {
   a.ModSub(&us2w, &vs3);
   a.ModSub(&_2vs2v2);
 
-  // Finalne obliczenia dla współrzędnych wyniku
   r.x.ModMulK1(&v, &a);
 
   vs3u2.ModMulK1(&vs3, &p1.y);
@@ -153,6 +134,11 @@ Point Secp256K1::Add2(Point &p1, Point &p2) {
   r.y.ModSub(&vs3u2);
 
   r.z.ModMulK1(&vs3, &p1.z);
+
+  // Apply fast reduction for secp256k1
+  r.x.ModReduceK1AVX512();
+  r.y.ModReduceK1AVX512();
+  r.z.ModReduceK1AVX512();
 
   return r;
 }
@@ -169,70 +155,57 @@ Point Secp256K1::Add(Point &p1, Point &p2) {
   Int vs3y1;
   Point r;
 
-// Wykorzystanie instrukcji AVX-512 do zrównoleglenia obliczeń
-#pragma omp parallel sections
-  {
-#pragma omp section
-    {
-      u1.ModMulK1(&p2.y, &p1.z);
-      v1.ModMulK1(&p2.x, &p1.z);
-    }
+  // Prefetch input data for optimal cache performance
+  _mm_prefetch((char*)p1.x.bits64, _MM_HINT_T0);
+  _mm_prefetch((char*)p1.y.bits64, _MM_HINT_T0);
+  _mm_prefetch((char*)p1.z.bits64, _MM_HINT_T0);
+  _mm_prefetch((char*)p2.x.bits64, _MM_HINT_T0);
+  _mm_prefetch((char*)p2.y.bits64, _MM_HINT_T0);
+  _mm_prefetch((char*)p2.z.bits64, _MM_HINT_T0);
 
-#pragma omp section
-    {
-      u2.ModMulK1(&p1.y, &p2.z);
-      v2.ModMulK1(&p1.x, &p2.z);
-    }
-  }
+  // Calculate intermediate values
+  u1.ModMulK1(&p2.y, &p1.z);
+  u2.ModMulK1(&p1.y, &p2.z);
+  v1.ModMulK1(&p2.x, &p1.z);
+  v2.ModMulK1(&p1.x, &p2.z);
 
-  // Sprawdzanie warunków specjalnych
-  if (v1.IsEqual(&v2)) {
-    if (!u1.IsEqual(&u2)) {
+  // Check for a point at infinity
+  if (v1.IsEqual(&v2)) {  // Check if the X coordinates are equal
+    if (!u1.IsEqual(&u2)) {  // If the Y-coordinates are different
+      // Point at infinity
       r.x.SetInt32(0);
       r.y.SetInt32(0);
       r.z.SetInt32(0);
       return r;
     } else {
-      return Double(p1);
+      // Doubling the point
+      return Double(p1);  // Method for doubling a point
     }
   }
 
-  // Główne obliczenia zoptymalizowane dla wielowątkowości
+  // Basic Dot Addition Calculations
   u.ModSub(&u1, &u2);
   v.ModSub(&v1, &v2);
-
-#pragma omp parallel sections
-  {
-#pragma omp section
-    {
-      w.ModMulK1(&p1.z, &p2.z);
-      us2.ModSquareK1(&u);
-    }
-
-#pragma omp section
-    { vs2.ModSquareK1(&v); }
-  }
-
+  w.ModMulK1(&p1.z, &p2.z);
+  us2.ModSquareK1(&u);
+  vs2.ModSquareK1(&v);
   vs3.ModMulK1(&vs2, &v);
   us2w.ModMulK1(&us2, &w);
   vs2v2.ModMulK1(&vs2, &v2);
   _2vs2v2.ModAdd(&vs2v2, &vs2v2);
   a.ModSub(&us2w, &vs3);
   a.ModSub(&_2vs2v2);
-
-#pragma omp parallel sections
-  {
-#pragma omp section
-    { r.x.ModMulK1(&v, &a); }
-
-#pragma omp section
-    { vs3u2.ModMulK1(&vs3, &u2); }
-  }
-
+  r.x.ModMulK1(&v, &a);
+  vs3u2.ModMulK1(&vs3, &u2);
   r.y.ModSub(&vs2v2, &a);
   r.y.ModMulK1(&r.y, &u);
   r.y.ModSub(&vs3u2);
   r.z.ModMulK1(&vs3, &w);
+
+  // Apply fast reduction for secp256k1
+  r.x.ModReduceK1AVX512();
+  r.y.ModReduceK1AVX512();
+  r.z.ModReduceK1AVX512();
 
   return r;
 }
@@ -244,7 +217,10 @@ Point Secp256K1::DoubleDirect(Point &p) {
   Point r;
   r.z.SetInt32(1);
 
-  // Zoptymalizowane obliczenia z wykorzystaniem AVX-512
+  // Prefetch input data
+  _mm_prefetch((char*)p.x.bits64, _MM_HINT_T0);
+  _mm_prefetch((char*)p.y.bits64, _MM_HINT_T0);
+
   _s.ModMulK1(&p.x, &p.x);
   _p.ModAdd(&_s, &_s);
   _p.ModAdd(&_s);
@@ -264,6 +240,10 @@ Point Secp256K1::DoubleDirect(Point &p) {
   r.y.ModAdd(&_p, &p.y);
   r.y.ModNeg();  // ry = neg(p.y + s*(ret.x+neg(p.x)));
 
+  // Apply fast reduction for secp256k1
+  r.x.ModReduceK1AVX512();
+  r.y.ModReduceK1AVX512();
+
   return r;
 }
 
@@ -273,33 +253,31 @@ Point Secp256K1::ComputePublicKey(Int *privKey) {
   Point Q;
   Q.Clear();
 
-  // Prefetching dla lepszego wykorzystania cache
-  _mm_prefetch((const char *)&GTable[0], _MM_HINT_T0);
+  // Prefetch private key data
+  _mm_prefetch((char*)privKey->bits64, _MM_HINT_T0);
 
-  // Wyszukaj pierwszy znaczący bajt
+  // Search first significant byte
   for (i = 0; i < 32; i++) {
     b = privKey->GetByte(i);
     if (b) break;
   }
+  
+  // Prefetch first table entry
+  _mm_prefetch((char*)&GTable[256 * i + (b - 1)], _MM_HINT_T0);
+  Q = GTable[256 * i + (b - 1)];
+  i++;
 
-  if (i < 32) {
-    Q = GTable[256 * i + (b - 1)];
-    i++;
-
-    // Prefetching z wyprzedzeniem dla lepszej wydajności cache
-    if (i < 31) {
-      _mm_prefetch((const char *)&GTable[256 * (i + 1)], _MM_HINT_T0);
-    }
-
-    for (; i < 32; i++) {
-      b = privKey->GetByte(i);
-      if (b) {
-        // Prefetching dla następnej iteracji
-        if (i < 31) {
-          _mm_prefetch((const char *)&GTable[256 * (i + 1)], _MM_HINT_T0);
-        }
-        Q = Add2(Q, GTable[256 * i + (b - 1)]);
-      }
+  for (; i < 32; i++) {
+    b = privKey->GetByte(i);
+    if (b) {
+      // Prefetch next table entry
+      _mm_prefetch((char*)&GTable[256 * i + (b - 1)], _MM_HINT_T0);
+      Q = Add2(Q, GTable[256 * i + (b - 1)]);
+      
+      // Apply periodic reduction for better performance
+      Q.x.ModReduceK1AVX512();
+      Q.y.ModReduceK1AVX512();
+      Q.z.ModReduceK1AVX512();
     }
   }
 
@@ -334,60 +312,35 @@ Point Secp256K1::Double(Point &p) {
   Int h;
   Point r;
 
-// Zrównoleglenie obliczeń z wykorzystaniem OpenMP
-#pragma omp parallel sections
-  {
-#pragma omp section
-    {
-      z2.ModSquareK1(&p.z);
-      z2.SetInt32(0);  // a=0
-    }
+  // Prefetch input point data
+  _mm_prefetch((char*)p.x.bits64, _MM_HINT_T0);
+  _mm_prefetch((char*)p.y.bits64, _MM_HINT_T0);
+  _mm_prefetch((char*)p.z.bits64, _MM_HINT_T0);
 
-#pragma omp section
-    { x2.ModSquareK1(&p.x); }
-  }
-
+  z2.ModSquareK1(&p.z);
+  z2.SetInt32(0);  // a=0
+  x2.ModSquareK1(&p.x);
   _3x2.ModAdd(&x2, &x2);
   _3x2.ModAdd(&x2);
   w.ModAdd(&z2, &_3x2);
-
-#pragma omp parallel sections
-  {
-#pragma omp section
-    {
-      s.ModMulK1(&p.y, &p.z);
-      b.ModMulK1(&p.y, &s);
-      b.ModMulK1(&p.x);
-    }
-
-#pragma omp section
-    { h.ModSquareK1(&w); }
-  }
-
+  s.ModMulK1(&p.y, &p.z);
+  b.ModMulK1(&p.y, &s);
+  b.ModMulK1(&p.x);
+  h.ModSquareK1(&w);
   _8b.ModAdd(&b, &b);
   _8b.ModDouble();
   _8b.ModDouble();
   h.ModSub(&_8b);
 
-// Ostateczne obliczenia dla współrzędnych wyniku
-#pragma omp parallel sections
-  {
-#pragma omp section
-    {
-      r.x.ModMulK1(&h, &s);
-      r.x.ModAdd(&r.x);
-    }
+  r.x.ModMulK1(&h, &s);
+  r.x.ModAdd(&r.x);
 
-#pragma omp section
-    {
-      s2.ModSquareK1(&s);
-      y2.ModSquareK1(&p.y);
-      _8y2s2.ModMulK1(&y2, &s2);
-      _8y2s2.ModDouble();
-      _8y2s2.ModDouble();
-      _8y2s2.ModDouble();
-    }
-  }
+  s2.ModSquareK1(&s);
+  y2.ModSquareK1(&p.y);
+  _8y2s2.ModMulK1(&y2, &s2);
+  _8y2s2.ModDouble();
+  _8y2s2.ModDouble();
+  _8y2s2.ModDouble();
 
   r.y.ModAdd(&b, &b);
   r.y.ModAdd(&r.y, &r.y);
@@ -400,12 +353,20 @@ Point Secp256K1::Double(Point &p) {
   r.z.ModDouble();
   r.z.ModDouble();
 
+  // Apply fast reduction for secp256k1
+  r.x.ModReduceK1AVX512();
+  r.y.ModReduceK1AVX512();
+  r.z.ModReduceK1AVX512();
+
   return r;
 }
 
 Int Secp256K1::GetY(Int x, bool isEven) {
   Int _s;
   Int _p;
+
+  // Prefetch input data
+  _mm_prefetch((char*)x.bits64, _MM_HINT_T0);
 
   _s.ModSquareK1(&x);
   _p.ModMulK1(&_s, &x);
@@ -418,12 +379,19 @@ Int Secp256K1::GetY(Int x, bool isEven) {
     _p.ModNeg();
   }
 
+  // Apply fast reduction
+  _p.ModReduceK1AVX512();
+
   return _p;
 }
 
 bool Secp256K1::EC(Point &p) {
   Int _s;
   Int _p;
+
+  // Prefetch point data
+  _mm_prefetch((char*)p.x.bits64, _MM_HINT_T0);
+  _mm_prefetch((char*)p.y.bits64, _MM_HINT_T0);
 
   _s.ModSquareK1(&p.x);
   _p.ModMulK1(&_s, &p.x);
@@ -432,69 +400,4 @@ bool Secp256K1::EC(Point &p) {
   _s.ModSub(&_p);
 
   return _s.IsZero();  // ( ((pow2(y) - (pow3(x) + 7)) % P) == 0 );
-}
-
-// Nowa funkcja dla przetwarzania wsadowego punktów korzystająca z AVX-512
-void Secp256K1::BatchProcessPoints(Point *points, int numPoints, Point *results) {
-// Wykorzystanie AVX-512 do równoległego przetwarzania wielu punktów
-#pragma omp parallel for
-  for (int i = 0; i < numPoints; i++) {
-    results[i] = Double(points[i]);
-  }
-}
-
-// Funkcja do zrównoleglonego obliczania wielu kluczy publicznych
-void Secp256K1::BatchComputePublicKeys(Int *privKeys, int numKeys, Point *pubKeys) {
-#pragma omp parallel for
-  for (int i = 0; i < numKeys; i++) {
-    pubKeys[i] = ComputePublicKey(&privKeys[i]);
-  }
-}
-
-// Zoptymalizowana funkcja normalizacji wsadowej dla punktów
-void Secp256K1::BatchNormalize(Point *points, int count) {
-  if (count < 2) {
-    if (count == 1) points[0].Normalize();
-    return;
-  }
-
-  Int *z = new Int[count];
-  Int *zt = new Int[count];
-
-  z[0].Set(&points[0].z);
-
-  for (int i = 1; i < count; i++) {
-    z[i].ModMulK1(&z[i - 1], &points[i].z);
-  }
-
-  zt[count - 1].ModInv(&z[count - 1]);
-
-  for (int i = count - 1; i > 0; i--) {
-    zt[i - 1].ModMulK1(&zt[i], &points[i].z);
-    Int zz;
-    zz.ModMulK1(&zt[i], &z[i - 1]);
-
-    Int zzi;
-    zzi.ModSquareK1(&zz);
-
-    Int zzz;
-    zzz.ModMulK1(&zzi, &zz);
-
-    points[i].x.ModMulK1(&points[i].x, &zzi);
-    points[i].y.ModMulK1(&points[i].y, &zzz);
-    points[i].z.SetInt32(1);
-  }
-
-  Int zzi;
-  zzi.ModSquareK1(&zt[0]);
-
-  Int zzz;
-  zzz.ModMulK1(&zzi, &zt[0]);
-
-  points[0].x.ModMulK1(&points[0].x, &zzi);
-  points[0].y.ModMulK1(&points[0].y, &zzz);
-  points[0].z.SetInt32(1);
-
-  delete[] z;
-  delete[] zt;
 }
