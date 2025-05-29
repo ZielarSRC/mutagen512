69003db6badcd096b84968e33247f8ee383faca5
Aleksander <poczta@zielar.pl>
2025-05-29 07:59:04 +0200

    Update Point.h

--- Point.h
+++ Point.h
@@ -4,26 +4,27 @@
 #include "Int.h"
 
 class Point {
-
-public:
-
+ public:
   Point();
-  Point(Int *cx,Int *cy,Int *cz);
+  Point(Int *cx, Int *cy, Int *cz);
   Point(Int *cx, Int *cz);
   Point(const Point &p);
+  // Dodane konstruktory przyjmujące referencje
+  Point(Int &cx, Int &cy, Int &cz);
+  Point(Int &cx, Int &cy);
   ~Point();
+
   bool isZero();
   bool equals(Point &p);
   void Set(Point &p);
-  void Set(Int *cx, Int *cy,Int *cz);
+  void Set(Int *cx, Int *cy, Int *cz);
   void Clear();
   void Reduce();
-  //std::string toString();
 
+  // Dane punktu
   Int x;
   Int y;
   Int z;
-
 };
 
-#endif // POINTH
+#endif  // POINTH
