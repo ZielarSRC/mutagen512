22accaccbbf61e753fe4dc24670f315ee64b9d3c
Aleksander <poczta@zielar.pl>
2025-05-29 07:23:57 +0200

    Update SECP256K1.h

--- SECP256K1.h
+++ SECP256K1.h
@@ -1,9 +1,10 @@
 #ifndef SECP256K1H
 #define SECP256K1H
 
+#include <immintrin.h>
+
 #include <string>
 #include <vector>
-#include <immintrin.h>
 
 #include "Point.h"
 
@@ -11,6 +12,9 @@
 #define P2PKH 0
 #define P2SH 1
 #define BECH32 2
+
+// Alignment for AVX-512 operations
+#define AVX512_ALIGNMENT 64
 
 class Secp256K1 {
  public:
@@ -23,18 +27,22 @@ class Secp256K1 {
   bool EC(Point &p);
   Int GetY(Int x, bool isEven);
 
-  void GetHash160(int type, bool compressed, Point &k0, Point &k1, Point &k2,
-                  Point &k3, uint8_t *h0, uint8_t *h1, uint8_t *h2,
-                  uint8_t *h3);
+  // Batch operations optimized for AVX-512
+  void BatchComputePublicKeys(Int *privKeys, Point *pubKeys, int batchSize);
 
-  void GetHash160(int type, bool compressed, Point &pubKey,
-                  unsigned char *hash);
+  // AVX-512 optimized versions of key operations
+  Point AddDirectAVX512(Point &p1, Point &p2);
+  Point DoubleDirectAVX512(Point &p);
+
+  void GetHash160(int type, bool compressed, Point &k0, Point &k1, Point &k2, Point &k3,
+                  uint8_t *h0, uint8_t *h1, uint8_t *h2, uint8_t *h3);
+
+  void GetHash160(int type, bool compressed, Point &pubKey, unsigned char *hash);
 
   std::string GetAddress(int type, bool compressed, Point &pubKey);
   std::string GetAddress(int type, bool compressed, unsigned char *hash160);
-  std::vector<std::string> GetAddress(int type, bool compressed,
-                                      unsigned char *h1, unsigned char *h2,
-                                      unsigned char *h3, unsigned char *h4);
+  std::vector<std::string> GetAddress(int type, bool compressed, unsigned char *h1,
+                                      unsigned char *h2, unsigned char *h3, unsigned char *h4);
   std::string GetPrivAddress(bool compressed, Int &privKey);
   std::string GetPublicKeyHex(bool compressed, Point &p);
   Point ParsePublicKeyHex(std::string str, bool &isCompressed);
@@ -43,6 +51,7 @@ class Secp256K1 {
 
   static Int DecodePrivateKey(char *key, bool *compressed);
 
+  // Standard operations
   Point Add(Point &p1, Point &p2);
   Point Add2(Point &p1, Point &p2);
   Point AddDirect(Point &p1, Point &p2);
@@ -55,7 +64,11 @@ class Secp256K1 {
  private:
   uint8_t GetByte(std::string &str, int idx);
 
-  Point GTable[256 * 32];  // Generator table
+  // Helper methods for AVX-512 optimizations
+  void PrefetchPoint(const Point &p, int hint = _MM_HINT_T0);
+  bool ShouldUsePrefetch(const Point &p1, const Point &p2);
+
+  alignas(AVX512_ALIGNMENT) Point GTable[256 * 32];  // Generator table aligned for AVX-512
 };
 
 #endif  // SECP256K1H
