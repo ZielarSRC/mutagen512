6757b71a32ebd48067b0eb944444b2d84d9d5c9e
Aleksander <poczta@zielar.pl>
2025-05-29 15:20:38 +0200

    Update SECP256K1.h

--- SECP256K1.h
+++ SECP256K1.h
@@ -1,13 +1,15 @@
 #ifndef SECP256K1_H
 #define SECP256K1_H
 
-#include <stdlib.h>
 #include <stdint.h>
-#include <string>
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <string>
+
 #include "Int.h"
-#include "Point.h"
 #include "IntGroup.h"
+#include "Point.h"
 
 // Secp256K1 Elliptic Curve
 // y^2 = x^3 + 7
@@ -17,16 +19,14 @@
 //                  483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8)
 
 class Secp256K1 {
-
-public:
-
+ public:
   Secp256K1();
   void Init();
   Point ComputePublicKey(Int *privKey);
   void Check();
   Int GetY(Int x, bool isEven);
   bool EC(Int &x, Int &y);
-  
+
   // Dodawanie i podwajanie punktów
   Point AddDirect(Point &p1, Point &p2);
   Point AddDirectAVX512(Point &p1, Point &p2);
@@ -34,20 +34,20 @@ public:
   Point DoubleDirectAVX512(Point &p);
   Point AddJacobian(Point &p1, Point &p2);
   Point DoubleJacobian(Point &p);
-  
+
   // Operacje na punktach
   Point NextKey(Point &key);
   bool PointAtInfinity(Point &p);
-  void PrefetchPoint(const Point& p, int hint);
-  
+  void PrefetchPoint(const Point &p, int hint);
+
   // Mnożenie skalarne
   Point ScalarMultiplication(Point &p, Int *scalar, bool isBatchMode = false);
-  
+
   // Operacje na podpisach
   bool VerifySignature(Int &hash, Int &r, Int &s, Point &pubKey);
   bool BatchVerify(int batchSize, Point *publicKeys, Int *hashes, Int *rs, Int *ss);
   void SNARK_Proof(Int &x, Int &y, Int &r);
-  
+
   // Kompresja i dekompresja punktów
   Point CompressPoint(Point &p);
   Point DecompressPoint(Point &compressedPoint);
@@ -58,4 +58,4 @@ public:
   Point G;    // Generator point
 };
 
-#endif // SECP256K1_H
+#endif  // SECP256K1_H
