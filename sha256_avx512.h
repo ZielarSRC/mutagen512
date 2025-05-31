22bcc0af8579c05dd062900d17a53723fa54b462
Aleksander <poczta@zielar.pl>
2025-05-31 03:16:45 +0200

    Update sha256_avx512.h

--- sha256_avx512.h
+++ sha256_avx512.h
@@ -3,31 +3,19 @@
 
 #include <cstdint>
 
-// AVX-512 optimized SHA-256 for 16 parallel computations
-void sha256avx512_16B(
-    const uint8_t* data0, const uint8_t* data1, const uint8_t* data2, const uint8_t* data3,
-    const uint8_t* data4, const uint8_t* data5, const uint8_t* data6, const uint8_t* data7,
-    const uint8_t* data8, const uint8_t* data9, const uint8_t* data10, const uint8_t* data11,
-    const uint8_t* data12, const uint8_t* data13, const uint8_t* data14, const uint8_t* data15,
-    unsigned char* hash0, unsigned char* hash1, unsigned char* hash2, unsigned char* hash3,
-    unsigned char* hash4, unsigned char* hash5, unsigned char* hash6, unsigned char* hash7,
-    unsigned char* hash8, unsigned char* hash9, unsigned char* hash10, unsigned char* hash11,
-    unsigned char* hash12, unsigned char* hash13, unsigned char* hash14, unsigned char* hash15
-);
+// SHA-256 implementation optimized for AVX-512 on Intel Xeon Platinum 8488C
+// Processes 16 message blocks in parallel using full 512-bit AVX-512 registers
 
-// Backward compatibility wrapper for 8 parallel computations
-void sha256avx512_8B(
-    const uint8_t* data0, const uint8_t* data1, const uint8_t* data2, const uint8_t* data3,
-    const uint8_t* data4, const uint8_t* data5, const uint8_t* data6, const uint8_t* data7,
-    unsigned char* hash0, unsigned char* hash1, unsigned char* hash2, unsigned char* hash3,
-    unsigned char* hash4, unsigned char* hash5, unsigned char* hash6, unsigned char* hash7
-);
+void sha256avx512_16B(const uint8_t* data0, const uint8_t* data1, const uint8_t* data2,
+                      const uint8_t* data3, const uint8_t* data4, const uint8_t* data5,
+                      const uint8_t* data6, const uint8_t* data7, const uint8_t* data8,
+                      const uint8_t* data9, const uint8_t* data10, const uint8_t* data11,
+                      const uint8_t* data12, const uint8_t* data13, const uint8_t* data14,
+                      const uint8_t* data15, unsigned char* hash0, unsigned char* hash1,
+                      unsigned char* hash2, unsigned char* hash3, unsigned char* hash4,
+                      unsigned char* hash5, unsigned char* hash6, unsigned char* hash7,
+                      unsigned char* hash8, unsigned char* hash9, unsigned char* hash10,
+                      unsigned char* hash11, unsigned char* hash12, unsigned char* hash13,
+                      unsigned char* hash14, unsigned char* hash15);
 
-// Batch processing for variable number of inputs
-void sha256avx512_batch(
-    const uint8_t** data_array, 
-    unsigned char** hash_array, 
-    int count
-);
-
-#endif // SHA256_AVX512_H
+#endif  // SHA256_AVX512_H
