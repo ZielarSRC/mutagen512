6cc5683ce8d187bf29a5a49353f66e9bca14b75a
Aleksander <poczta@zielar.pl>
2025-05-31 17:15:18 +0200

    Update sha256_avx512.h

--- sha256_avx512.h
+++ sha256_avx512.h
@@ -1,19 +1,10 @@
 #ifndef SHA256_AVX512_H
 #define SHA256_AVX512_H
 
-#include <immintrin.h>
-
 #include <cstdint>
 
-namespace sha256avx512 {
-
-// Initialize SHA-256 state for 16 parallel computations
-void Initialize(__m512i* state);
-
-// Transform 16 parallel SHA-256 blocks using AVX-512
-void Transform(__m512i* state, const uint8_t* data[16]);
-
-// Main hashing function for 16 messages of 64 bytes each
+// Optimized SHA-256 implementation for Xeon Platinum 8488C with AVX-512
+// Processes 16 message blocks in parallel
 void sha256avx512_16B(
     const uint8_t* data0, const uint8_t* data1, const uint8_t* data2,
     const uint8_t* data3, const uint8_t* data4, const uint8_t* data5,
@@ -26,7 +17,5 @@ void sha256avx512_16B(
     unsigned char* hash8, unsigned char* hash9, unsigned char* hash10,
     unsigned char* hash11, unsigned char* hash12, unsigned char* hash13,
     unsigned char* hash14, unsigned char* hash15);
-
-}  // namespace sha256avx512
 
 #endif  // SHA256_AVX512_H
