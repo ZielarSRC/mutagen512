87a49db2f0dd06fe68cd239a7918067a603a8b3a
Aleksander <poczta@zielar.pl>
2025-05-31 14:25:18 +0200

    Update sha256_avx512.h

--- sha256_avx512.h
+++ sha256_avx512.h
@@ -5,17 +5,17 @@
 
 // SHA-256 implementation optimized for AVX-512 on Intel Xeon Platinum 8488C
 // Processes 16 message blocks in parallel using full 512-bit AVX-512 registers
-
-void sha256avx512_16B(const uint8_t* data0, const uint8_t* data1, const uint8_t* data2,
-                      const uint8_t* data3, const uint8_t* data4, const uint8_t* data5,
-                      const uint8_t* data6, const uint8_t* data7, const uint8_t* data8,
-                      const uint8_t* data9, const uint8_t* data10, const uint8_t* data11,
-                      const uint8_t* data12, const uint8_t* data13, const uint8_t* data14,
-                      const uint8_t* data15, unsigned char* hash0, unsigned char* hash1,
-                      unsigned char* hash2, unsigned char* hash3, unsigned char* hash4,
-                      unsigned char* hash5, unsigned char* hash6, unsigned char* hash7,
-                      unsigned char* hash8, unsigned char* hash9, unsigned char* hash10,
-                      unsigned char* hash11, unsigned char* hash12, unsigned char* hash13,
-                      unsigned char* hash14, unsigned char* hash15);
+void sha256avx512_16B(
+    const uint8_t* data0, const uint8_t* data1, const uint8_t* data2,
+    const uint8_t* data3, const uint8_t* data4, const uint8_t* data5,
+    const uint8_t* data6, const uint8_t* data7, const uint8_t* data8,
+    const uint8_t* data9, const uint8_t* data10, const uint8_t* data11,
+    const uint8_t* data12, const uint8_t* data13, const uint8_t* data14,
+    const uint8_t* data15, unsigned char* hash0, unsigned char* hash1,
+    unsigned char* hash2, unsigned char* hash3, unsigned char* hash4,
+    unsigned char* hash5, unsigned char* hash6, unsigned char* hash7,
+    unsigned char* hash8, unsigned char* hash9, unsigned char* hash10,
+    unsigned char* hash11, unsigned char* hash12, unsigned char* hash13,
+    unsigned char* hash14, unsigned char* hash15);
 
 #endif  // SHA256_AVX512_H
