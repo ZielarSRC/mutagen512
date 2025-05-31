a14cd27749665e0c316f872f030e2cead11d14cd
Aleksander <poczta@zielar.pl>
2025-05-31 17:16:19 +0200

    Update ripemd160_avx512.h

--- ripemd160_avx512.h
+++ ripemd160_avx512.h
@@ -7,26 +7,27 @@
 
 namespace ripemd160avx512 {
 
-// Initialize RIPEMD-160 state for 16 parallel computations
-void Initialize(__m512i* state);
+// Initialize state with AVX-512
+void Initialize(__m512i *state);
 
-// Transform 16 parallel RIPEMD-160 blocks
-void Transform(__m512i* state, uint8_t* blocks[16]);
+// Transform function for AVX-512 (16 blocks in parallel)
+void Transform(__m512i *state, uint8_t *blocks[16]);
 
-// Main hashing function for 16 messages of 32 bytes each
-void ripemd160avx512_32(unsigned char* i0, unsigned char* i1, unsigned char* i2,
-                        unsigned char* i3, unsigned char* i4, unsigned char* i5,
-                        unsigned char* i6, unsigned char* i7, unsigned char* i8,
-                        unsigned char* i9, unsigned char* i10,
-                        unsigned char* i11, unsigned char* i12,
-                        unsigned char* i13, unsigned char* i14,
-                        unsigned char* i15, unsigned char* d0,
-                        unsigned char* d1, unsigned char* d2, unsigned char* d3,
-                        unsigned char* d4, unsigned char* d5, unsigned char* d6,
-                        unsigned char* d7, unsigned char* d8, unsigned char* d9,
-                        unsigned char* d10, unsigned char* d11,
-                        unsigned char* d12, unsigned char* d13,
-                        unsigned char* d14, unsigned char* d15);
+// Main hashing function optimized for Intel Xeon Platinum 8488C
+// Processes 16 messages of 32 bytes each in parallel
+void ripemd160avx512_32(unsigned char *i0, unsigned char *i1, unsigned char *i2,
+                        unsigned char *i3, unsigned char *i4, unsigned char *i5,
+                        unsigned char *i6, unsigned char *i7, unsigned char *i8,
+                        unsigned char *i9, unsigned char *i10,
+                        unsigned char *i11, unsigned char *i12,
+                        unsigned char *i13, unsigned char *i14,
+                        unsigned char *i15, unsigned char *d0,
+                        unsigned char *d1, unsigned char *d2, unsigned char *d3,
+                        unsigned char *d4, unsigned char *d5, unsigned char *d6,
+                        unsigned char *d7, unsigned char *d8, unsigned char *d9,
+                        unsigned char *d10, unsigned char *d11,
+                        unsigned char *d12, unsigned char *d13,
+                        unsigned char *d14, unsigned char *d15);
 
 }  // namespace ripemd160avx512
 
