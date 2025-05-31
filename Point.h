c2ae6989c38122fa55e74814e6957e64d335f2c1
Aleksander <poczta@zielar.pl>
2025-05-31 03:20:37 +0200

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,19 +1,21 @@
 #ifndef POINTH
 #define POINTH
 
+#include <immintrin.h>  // For AVX-512 intrinsics
+
 #include "Int.h"
 
-class Point {
+// Point class optimized for AVX-512 operations on Intel Xeon Platinum 8488C
+class alignas(64) Point {
  public:
+  // Constructors optimized for AVX-512
   Point();
   Point(Int *cx, Int *cy, Int *cz);
   Point(Int *cx, Int *cz);
   Point(const Point &p);
-  // Dodane konstruktory przyjmujące referencje
-  Point(Int &cx, Int &cy, Int &cz);
-  Point(Int &cx, Int &cy);
   ~Point();
 
+  // Fast AVX-512 optimized operations
   bool isZero();
   bool equals(Point &p);
   void Set(Point &p);
@@ -21,10 +23,14 @@ class Point {
   void Clear();
   void Reduce();
 
-  // Dane punktu
-  Int x;
-  Int y;
-  Int z;
+  // AVX-512 optimized coordinate storage
+  alignas(64) Int x;
+  alignas(64) Int y;
+  alignas(64) Int z;
+
+  // Batch processing methods for AVX-512
+  static void BatchReduce(Point *points, int count);
+  static void BatchEquals(Point *points1, Point *points2, bool *results, int count);
 };
 
 #endif  // POINTH
